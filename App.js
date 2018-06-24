605e860d182e401592473e04287dfa015739b64f
suresh kumara <kumara.suresh25@gmail.com>
2018-06-24 11:50:26 +0530

    latest changes

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items: [
-        {
-            xtype: 'container',
-            itemId: 'exportButton'
-        }],
 
  //   ************* My Code *******************
     launch: function() {
       this._loadMilestones();
+    },
+
+    _callConfluence: function() {
+        var grid = this.down('rallygrid'),
+              store = grid.getStore();
+        var  items = store.data.items;
+        var result = "<table>";
+        result += "<thead><th><td>Issue Title</td><td>Issue Description</td></th></thead><tbody>";
+        for(var i=0; i<items.length; i++) {
+          result += "<tr><td>" + this._html2text(items[i].data.Name) + "</td><td>" + this._html2text(items[i].data.Description) + "</td></tr>";
+        }
+        result += "</tbody></table>";
+        console.log(result.replace(/\s/g, ' '));
+    },
+
+    _loadMilestones: function() {
       exportToExcel = {
                   xtype: 'rallybutton',
                   handler: this._callConfluence,
@@ -25,69 +36,8 @@ Ext.define('CustomApp', {
                   },
                   scope: this
               };
-              this.down('#exportButton').add(exportToExcel);
-      console.log("Inside 'Launch")
-    },
+      this.add(exportToExcel);
 
-    _callConfluence: function() {
-        var grid = this.down('rallygrid'),
-              store = grid.getStore();
-             console.log("Inside confluence csll");
-             console.log(store);
-             console.log("Inside confluence csll2");
-             console.log(store.data);
-           var  items = store.data.items;
-           var loop = [];
-        for(var i=0; i<items.length; i++) {
-          loop.push({title: items[i].data.Name, description: items[i].data.Description});
-        }
-        var data = JSON.stringify({array: loop});
-        console.log(data);
-      //  this._createConfluencePage();
-    },
-
-    _createConfluencePage : function() {
-      // console.log("inside confluence page");
-      // var url = "https://docops-temp.ca.com/rest/api/content";
-      //
-      // var data = {
-      //             "type":"page",
-      //             "title":"My Test Page",
-      //         "space": {
-      //             "key":"SEOT"
-      //                   },
-      //                   "body": {
-      //                 "storage": {
-      //                       "value":"<p>This is a new page</p>",
-      //                       "representation":"storage"
-      //                 }
-      //         }
-      // };
-
-      // Ext.Ajax.request({
-      //       url:url,
-      //       method: 'POST',
-      //       jsonData: data,
-      //       useDefaultXhrHeader: false,
-      //       withCredentials: true,
-      //        headers : {
-      //            'Authorization': 'Basic U2hhbmtlcjpjaGlydQ==',
-      //             'Content-Type': 'application/json;charset=UTF-8',
-      //        },
-      //       success: function(response) {
-      //             console.log("successful");
-      //       },
-      //       failure: function(response) {
-      //             console.log("failures");
-      //       }
-      // });
-
-//      console.log("outside");
-
-    },
-
-    _loadMilestones: function() {
-      console.log("Inside 'Milestone")
       this.milestoneComboBox = Ext.create('Rally.ui.combobox.MilestoneComboBox',{
             fieldLabel: 'Milestone',
             hideLabel: false,
@@ -96,8 +46,7 @@ Ext.define('CustomApp', {
                   ready: function(combobox) {
                         this._loadData();
                   },
-
-                        select: function(combobox, records) {
+                  select: function(combobox, records) {
                         this._loadData();
                   },
             //    success: this._onStoreBuilt,
@@ -109,9 +58,7 @@ Ext.define('CustomApp', {
 
     //Get data from rally
     _loadData: function() {
-      console.log("Inside Load")
       var selectedMilestone = this.milestoneComboBox.getRecord().get('_ref');
-      //console.log("Selected Milestone", selectedMilestone);
       var defectfilters = [
           {
             property: 'Milestones',
@@ -144,20 +91,23 @@ Ext.define('CustomApp', {
       },
       fetch: ['FormattedID', 'Name', 'Description', 'AffectsDoc']
       });
-      console.log("Store Data :",this.defectStore);
       }
     },
 
     //Show the data fetched in a grid
     _createGrid: function(myDefectStore) {
-      console.log("Inside 'Grid")
       this.myGrid = Ext.create('Rally.ui.grid.Grid', {
                   store: myDefectStore,
                   columnCfgs: [
                         'FormattedID', 'Name', 'Description', 'AffectsDoc'
                   ]
         });
-     console.log('my grid', this.myGrid);
      this.add(this.myGrid);
+    },
+    // strip html from string
+    _html2text: function(html) {
+      var tag = document.createElement('div');
+      tag.innerHTML = html;
+      return tag.innerText;
     },
 });
